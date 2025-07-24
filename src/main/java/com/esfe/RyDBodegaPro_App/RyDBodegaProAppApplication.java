eee431c58874fa239e92b5fac4e15c1e2c55dac2
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-07-24 09:28:34 -0600

    Se cambio la Inicializacion de proyecto por presentar un error anteriormente

---
+++ src/main/java/com/esfe/RyDBodegaPro_App/RyDBodegaProAppApplication.java
@@ -0,0 +1,13 @@
+package com.esfe.RyDBodegaPro_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyDBodegaProAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyDBodegaProAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esfe/RyDBodegaPro_App/RyDBodegaProAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esfe.RyDBodegaPro_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyDBodegaProAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
